31485f33edff282a348100e24353dc6fc81b3fd7
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2021-11-30 10:23:31 -0300

    feat: add services in server

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
+const tasks = require('./task');
 
 const packageDefs = protoLoader.loadSync('./tasks.proto');
-
 const taskProto = grpc.loadPackageDefinition(packageDefs);
 
 const gRpcServer = new grpc.Server();
+
+gRpcServer.addService(taskProto.TaskService.service, {
+  findAll: (_, callback) => {
+    callback(null, { tasks });
+  },
+  find: (_, callback) => {
+    const task = tasks.find(el => el.id == _request.id);
+    if (task) {
+      callback(null, { task });
+    }
+    callback({
+      code: grpc.status.NOT_FOUND,
+      message: 'Id does not exists'
+    });
+  },
+  insert: (_, callback) => {
+    const task = _.request;
+    tasks.push(task);
+    callback(null, { task });
+  },
+});
 
 gRpcServer.bind(
   '127.0.0.1:9000',
   grpc.ServerCredentials.createInsecure()
 );
 gRpcServer.start();
+console.log('gRPC Server has been started.. http://127.0.0.1:9000');
